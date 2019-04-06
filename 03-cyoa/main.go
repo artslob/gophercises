cc1447d89d3d86db77eb69aa33766b927f28f175
artslob <artslob@yandex.ru>
2019-04-06 14:44:25 +0300

    first version of story handler

--- 03-cyoa/main.go
+++ 03-cyoa/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/artslob/gophercises/03-cyoa/story"
 	"io/ioutil"
+	"net/http"
 )
 
 func checkError(err error) {
@@ -24,5 +25,20 @@ func main() {
 	checkError(err)
 
 	fmt.Println("Story successfully parsed")
-	fmt.Println(mainStory["home"].Options)
+
+	port := ":8888"
+	fmt.Printf("Starting the server on http://127.1%s\n", port)
+	err = http.ListenAndServe(port, storyHandler(mainStory))
+	checkError(err)
+}
+
+func storyHandler(mainStory story.Story) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		path := request.URL.Path
+		if a, ok := mainStory[path[1:]]; ok {
+			_, _ = writer.Write([]byte(a.Title))
+		} else {
+			_, _ = writer.Write([]byte("How did you get here?"))
+		}
+	}
 }
