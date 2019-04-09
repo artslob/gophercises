5553133ff9de853b0e4839acb2e2645200d0b5b5
artslob <artslob@yandex.ru>
2019-04-09 20:30:36 +0300

    implement link parser

---
+++ 04-link/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"strings"
+)
+
+type Link struct {
+	Href string
+	Text strings.Builder
+}
+
+type Walker struct {
+	Links []Link
+}
+
+func (w *Walker) Walk(node *html.Node) {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		newLink := Link{}
+		for _, attr := range node.Attr {
+			if attr.Key == "href" {
+				newLink.Href = attr.Val
+				break
+			}
+		}
+		w.getText(node, &newLink.Text)
+		w.Links = append(w.Links, newLink)
+		return
+	}
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		w.Walk(child)
+	}
+}
+
+func (w *Walker) getText(node *html.Node, builder *strings.Builder) {
+	if node.Type == html.TextNode {
+		builder.WriteString(node.Data)
+	}
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		w.getText(child, builder)
+	}
+
+}
+
+func main() {
+	text := `
+		<p>Links:</p>
+		<ul>
+			<li><a href="/first">Foo <b>bold</b></a></li>
+			<li><a href="/second">BarBaz <span>in span <b>bold<b/></span> <b>test</b></a></li>
+		</ul>
+	`
+	r := strings.NewReader(text)
+	doc, err := html.Parse(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	walker := Walker{}
+	walker.Walk(doc)
+	for _, link := range walker.Links {
+		fmt.Println("link:", link.Href)
+		fmt.Println("text:", link.Text.String())
+	}
+}
