24d5cac2da1131dc9a75f24605b306d7371dda47
artslob <artslob@yandex.ru>
2019-04-18 12:38:53 +0300

    move checking input from person to goroutine

--- 01-quiz/main.go
+++ 01-quiz/main.go
@@ -51,6 +51,25 @@ func shuffleQuizzes(slice []Quiz) {
 	}
 }
 
+func checkQuiz(sc *bufio.Scanner, problemNo int, quiz Quiz, solved chan<- bool) {
+	fmt.Printf("Problem #%d: %s = ", problemNo, quiz.input)
+	if !sc.Scan() && sc.Err() != nil {
+		log.Fatal("error while reading input")
+	}
+	got := strings.TrimSpace(sc.Text())
+	if got == "" {
+		solved <- false
+		return
+	}
+	parsedGot, err := strconv.Atoi(got)
+	if err != nil {
+		fmt.Println("Expected integer. Going to the next question!")
+		solved <- false
+		return
+	}
+	solved <- parsedGot == quiz.answer
+}
+
 func main() {
 	filename := flag.String("csv", "problems.csv", "a csv file in the format 'question,answer'")
 	shuffle := flag.Bool("shuffle", true, `set to false if dont want shuffled quizzes: '-shuffle=false'`)
@@ -62,23 +81,12 @@ func main() {
 		shuffleQuizzes(quizzes)
 	}
 
-	scanner := bufio.NewScanner(os.Stdin)
+	sc := bufio.NewScanner(os.Stdin)
 	solved := 0
 	for i, quiz := range quizzes {
-		fmt.Printf("Problem #%d: %s = ", i+1, quiz.input)
-		if !scanner.Scan() && scanner.Err() != nil {
-			log.Fatal("error while reading input")
-		}
-		got := strings.TrimSpace(scanner.Text())
-		if got == "" {
-			continue
-		}
-		parsedGot, err := strconv.Atoi(got)
-		if err != nil {
-			fmt.Println("Expected integer. Going to the next question!")
-			continue
-		}
-		if parsedGot == quiz.answer {
+		solvedChannel := make(chan bool)
+		go checkQuiz(sc, i+1, quiz, solvedChannel)
+		if <-solvedChannel {
 			solved++
 		}
 	}
