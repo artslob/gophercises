db008a2bbf9136a6cd247fc7f3415c5351165a59
artslob <artslob@yandex.ru>
2019-04-23 23:34:38 +0300

    store database in home dir

--- 07-task/main.go
+++ 07-task/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/artslob/gophercises/07-task/cmd"
 	"github.com/boltdb/bolt"
 	"log"
+	"os"
 )
 
 func main() {
-	db, err := bolt.Open("my.db", 0600, nil)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := bolt.Open(fmt.Sprintf("%s/.todo-list.bolt.db", home), 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
