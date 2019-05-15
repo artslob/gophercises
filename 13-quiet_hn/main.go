d84f161294487d5e8cd2dc781251975846572e0b
artslob <artslob@yandex.ru>
2019-05-15 20:49:58 +0300

    concurrently get 30 stories in workers

--- 13-quiet_hn/main.go
+++ 13-quiet_hn/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 )
 
-const API = "https://hacker-news.firebaseio.com/v0/"
-
-var NewStoriesURL = API + "newstories.json"
+const (
+	API           = "https://hacker-news.firebaseio.com/v0/"
+	NewStoriesURL = API + "newstories.json"
+	ItemsURL      = API + "item/%d.json"
+)
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", root())
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", root()); err != nil {
 		log.Fatalf("got error: %v\n", err)
 	}
 }
@@ -36,7 +38,82 @@ func root() http.HandlerFunc {
 			_, _ = fmt.Fprintf(w, "Some error occurred while processing request: %q", err)
 			return
 		}
-		body := string(bodyBytes)
-		_, _ = fmt.Fprint(w, body)
+		ids := []int{}
+		if err := json.Unmarshal(bodyBytes, &ids); err != nil {
+			_, _ = fmt.Fprintf(w, "Could not parse response: %q", err)
+		}
+		_, _ = fmt.Fprintf(w, "ids %v", ids[:30])
+		in := gen(ids[:30]...)
+		storiesChannels := []<-chan StoryResponse{}
+		for range ids {
+			storiesChannels = append(storiesChannels, getStories(in))
+		}
+		for n := range merge(storiesChannels...) {
+			fmt.Println(n.Id)
+		}
 	}
+}
+
+type StoryResponse struct {
+	Story
+	err error
+}
+
+func merge(storiesChannels ...<-chan StoryResponse) <-chan StoryResponse {
+	out := make(chan StoryResponse)
+
+	var wg sync.WaitGroup
+	wg.Add(len(storiesChannels))
+
+	for _, c := range storiesChannels {
+		go func(c <-chan StoryResponse) {
+			for n := range c {
+				out <- n
+			}
+			wg.Done()
+		}(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func getStories(in <-chan int) <-chan StoryResponse {
+	out := make(chan StoryResponse)
+	go func() {
+		for n := range in {
+			resp, err := http.Get(fmt.Sprintf(ItemsURL, n))
+			if err != nil {
+				out <- StoryResponse{err: err}
+				continue
+			}
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			_ = resp.Body.Close()
+			if err != nil {
+				out <- StoryResponse{err: err}
+				continue
+			}
+			var story Story
+			if err := json.Unmarshal(bodyBytes, &story); err != nil {
+				out <- StoryResponse{err: err}
+				continue
+			}
+			out <- StoryResponse{Story: story}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
 }
---
+++ 13-quiet_hn/story.go
@@ -0,0 +1,34 @@
+package main
+
+type Story struct {
+	// The item's unique id.
+	Id int64 `json:"id"`
+	// true if the item is deleted.
+	Deleted bool `json:"deleted,omitempty"`
+	// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+	Type string `json:"type"`
+	// The username of the item's author.
+	By string `json:"by"`
+	// Creation date of the item, in Unix Time.
+	Time int64 `json:"time"`
+	// The comment, story or poll text. HTML.
+	Text string `json:"text,omitempty"`
+	// true if the item is dead.
+	Dead bool `json:"dead,omitempty"`
+	// The comment's parent: either another comment or the relevant story.
+	Parent int64 `json:"parent,omitempty"`
+	// The pollopt's associated poll.
+	Poll int64 `json:"poll,omitempty"`
+	// The ids of the item's comments, in ranked display order.
+	Kids []int64 `json:"kids"`
+	// The URL of the story.
+	Url string `json:"url"`
+	// The story's score, or the votes for a pollopt.
+	Score int64 `json:"score"`
+	// The title of the story, poll or job.
+	Title string `json:"title"`
+	// A list of related pollopts, in display order.
+	Parts []int64 `json:"parts,omitempty"`
+	// In the case of stories or polls, the total comment count.
+	Descendants int64 `json:"descendants"`
+}
