d466cd034fe6257c93b644baaf5de7792c5d80bb
artslob <artslob@yandex.ru>
2019-05-17 13:29:39 +0300

    add custom file server

--- 15-recover_chroma/files.go
+++ 15-recover_chroma/files.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	pathPackage "path"
 )
 
 func ServeFiles(mux *http.ServeMux, pattern string, addDefaultPaths bool, paths ...string) {
@@ -25,6 +30,44 @@ func ServeFiles(mux *http.ServeMux, pattern string, addDefaultPaths bool, paths 
 	}
 	for _, path := range allPaths {
 		prefix := pattern + path + "/"
-		mux.Handle(prefix, http.StripPrefix(prefix, http.FileServer(http.Dir(path))))
+		mux.Handle(prefix, http.StripPrefix(prefix, &SourceFileHandler{http.Dir(path)}))
 	}
+}
+
+type SourceFileHandler struct {
+	root http.FileSystem
+}
+
+func (h *SourceFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := pathPackage.Clean(r.URL.Path)
+	log.Print(name)
+	content, err := h.readFile(name)
+	if err != nil {
+		log.Println(err)
+		http.NotFound(w, r)
+		return
+	}
+	_, _ = w.Write(content)
+}
+
+func (h *SourceFileHandler) readFile(name string) ([]byte, error) {
+	f, err := h.root.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+
+	info, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if !info.Mode().IsRegular() {
+		return nil, errors.New(fmt.Sprintf("%q is not a regular file", name))
+	}
+
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
 }
